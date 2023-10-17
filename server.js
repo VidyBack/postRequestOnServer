1784ab0e12a0d0e47a54d036f83864d9108e2569
Junaidkhan1809 <72540582+Junaidkhan1809@users.noreply.github.com>
2023-10-17 16:45:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = low(adapter);
 const customHeadersMiddleware = (req, res, next) => {
   // Set custom headers
   res.setHeader('Cache-Control', 'public, max-age=3600');
-  res.setHeader('Cf-Cache-Status:', 'max-age=3600');
+  res.setHeader('Cf-Cache-Control:', 'max-age=3600');
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('Referrer-Policy', 'same-origin');
   res.setHeader('abc', 'XYZ123');
