c6a65f4210dd138e649cffaef1eaba2564fa8288
Junaidkhan1809 <72540582+Junaidkhan1809@users.noreply.github.com>
2023-10-18 12:55:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const db = low(adapter);
 const customHeadersMiddleware = (req, res, next) => {
   // Set custom headers
   res.setHeader('Cache-Control', 'public, max-age=3600');
-  res.setHeader('X-Frame-Options', 'DENY');
-  res.setHeader('Referrer-Policy', 'same-origin');
   res.setHeader('abc', 'XYZ123');
 
   // Continue with the next middleware or route handler
