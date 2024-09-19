f0e5a4970673a889e4835f5ca73796de7f653aca
GopalKuchnaya <98996752+GopalKuchnaya@users.noreply.github.com>
2024-09-19 17:04:32 +0530

    updated p_ps

--- server.js
+++ server.js
@@ -1,38 +1,55 @@
-
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync('db.json');
+const adapter = new FileSync("db.json");
 const db = low(adapter);
 
 const customHeadersMiddleware = (req, res, next) => {
   // Set custom headers
-  res.setHeader('Cache-Control', 'public, max-age=3600');
-  res.setHeader('abc', 'XYZ123');
+  res.setHeader("Cache-Control", "public, max-age=3600");
+  res.setHeader("abc", "XYZ123");
 
   // Continue with the next middleware or route handler
   next();
 };
+
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(customHeadersMiddleware);
 
-server.post('/:purpose', (req, res) => {
+// Function to update records by IDs and add a key-value pair
+const updateRecordsWithKeyValue = (ids, key, value, purpose) => {
+  ids.forEach((id) => {
+    db.get(purpose) // Replace with your actual collection name in db.json
+      .find({ id }) // Find the record with the specified id
+      .assign({ [key]: value }) // Add the key-value pair
+      .write(); // Write the changes to the db
+  });
+};
+updateRecordsWithKeyValue([
+  "2SO2ogihkPw9_6_Y43Dk6yI2",
+  "0EbjMEu9EUoyPFYuNqyKo",
+  "p65LnWZuyAnXQYUoiJcwb",
+  "3axwGvJ0wP64FO9TT4Hp8",
+  
+  
+  
+  ],
+"speedX",true,
+"p_ps");
 
+server.post("/:purpose", (req, res) => {
   // Process the request body and add/update data in db.json using lowdb
-  // For example:
   const newData = req.body;
- // Add a new post
-db.get(req.params.purpose).push(newData).write();
-db.write()
-console.log('Added new post:', newData);
-res.status(200).json(newData);
+  // Add a new post
+  db.get(req.params.purpose).push(newData).write();
+  console.log("Added new post:", newData);
+  res.status(200).json(newData);
 });
-
 
 server.use(router);
 
